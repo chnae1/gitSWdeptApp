86f6abba4b5af17280403a38bc93f206fb38e55e
chane1 <pchi0528@naver.com>
2021-05-25 21:53:32 +0900

    로그인/회원가입

---
+++ java/Intro.java
@@ -0,0 +1,30 @@
+package com.example.square;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Intro extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro_activity); //xml , java 소스 연결
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent = new Intent (getApplicationContext(), Login.class);
+                startActivity(intent); //다음화면으로 넘어감
+                finish();
+            }
+        },3000); //3초 뒤에 Runner객체 실행하도록 함
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+}
